9535bb2f2bdd8af000a20e79ba227df54ede507d
NguyenMinhThuan1704 <thuanpro9b@gmail.com>
2023-09-21 09:07:43 +0700

    upload source code

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+// var inputElement = document.querySelector('pass');
+
+// var placeholderValues = [
+//   'Bạn cần tìm gì...',
+//   'Đèn trang trí nội thất',
+//   'Đèn trang trí ngoại thất',
+// ];
+
+// var currentIndex = 1;
+
+// // Hàm để thay đổi giá trị placeholder và cập nhật chỉ số
+// function changePlaceholder() {
+//   inputElement.placeholder = placeholderValues[currentIndex];
+//   currentIndex = (currentIndex + 1) % placeholderValues.length;
+// }
+// setInterval(changePlaceholder, 4000);
