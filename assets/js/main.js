0ed0e3dee1267c475e18813f4a34a525ed458845
NguyenMinhThuan1704 <thuanpro9b@gmail.com>
2023-10-03 21:49:09 +0700

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,18 @@
 //   currentIndex = (currentIndex + 1) % placeholderValues.length;
 // }
 // setInterval(changePlaceholder, 4000);
+
+
+//-------------------slider-new-product----------------
+const rightbtn = document.querySelector('.btn-right')
+const leftbtn = document.querySelector('.fa-angle-left')
+const newproductlength = document.querySelector('.new__product-item').offsetWidth
+const sildeWrapper = document.querySelector('.new__product-item-wrapper')
+
+rightbtn.addEventListener("click", function(){
+    sildeWrapper.scrollLeft += newproductlength
+})
+
+leftbtn.addEventListener("click", function(){
+    sildeWrapper.scrollLeft -= newproductlength
+})
